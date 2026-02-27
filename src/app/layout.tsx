44a988d274ba2220ad39f73836df41759253b3ef
emilrichardo <emilrichardo@gmail.com>
2026-02-27 08:49:09 -0300

    feat: Introduce Tiempos font family and apply it as the primary heading font, replacing the previous serif font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { Source_Code_Pro, Inter, Lora } from "next/font/google";
+import { Source_Code_Pro, Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { Sidebar } from "@/components/Sidebar";
@@ -10,7 +11,23 @@ const sourceCodePro = Source_Code_Pro({
   variable: "--font-mono",
 });
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const lora = Lora({ subsets: ["latin"], variable: "--font-serif" });
+const tiemposHeadline = localFont({
+  src: [
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Light.otf", weight: "300", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-LightItalic.otf", weight: "300", style: "italic" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Regular.otf", weight: "400", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-RegularItalic.otf", weight: "400", style: "italic" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Medium.otf", weight: "500", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-MediumItalic.otf", weight: "500", style: "italic" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Semibold.otf", weight: "600", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-SemiboldItalic.otf", weight: "600", style: "italic" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Bold.otf", weight: "700", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-BoldItalic.otf", weight: "700", style: "italic" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-Black.otf", weight: "900", style: "normal" },
+    { path: "./fonts/tiempos/headline/TiemposHeadline-BlackItalic.otf", weight: "900", style: "italic" },
+  ],
+  variable: "--font-tiempos-headline",
+});
 // ... (metadata)
 
 import { LayoutContent } from "@/components/LayoutContent";
@@ -28,7 +45,7 @@ export default function RootLayout({
         }}
       />
       <body
-        className={`${sourceCodePro.variable} ${inter.variable} ${lora.variable} font-sans bg-background`}
+        className={`${sourceCodePro.variable} ${inter.variable} ${tiemposHeadline.variable} font-sans bg-background`}
         suppressHydrationWarning
       >
         <AuthProvider>
